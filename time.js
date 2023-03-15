66a01beee25806af17cb8768dec157c2bcfc9311
sherhard <goeasysher@gmail.com>
2023-03-15 23:29:48 +0800

    new timer

--- time.js
+++ time.js
@@ -5,10 +5,16 @@ let myTime=document.getElementById('myTime');
 	function stop(){
 		window.clearInterval(window.timerId)
 	}
+	let numberTimer=document.getElementById('numberTimer');
+			numberTimer.addEventListener('mouseout',timerNumber);
+			function timerNumber(){
+				timer.innerHTML=numberTimer.value;
+			}
 	function funcTime(){
 		window.timerId=window.setInterval(timers,300)
 		function timers(){
 			let timer=document.getElementById('timer');
+
 		let num=parseInt(timer.innerHTML)-1;
 		timer.innerHTML=num;
 		if(num==0){

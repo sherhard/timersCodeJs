6ff01e1eceb7d7eba9d0c7cd9731c1b899a45f39
sherhard <goeasysher@gmail.com>
2023-03-19 00:42:50 +0800

    add new button stop

--- time.js
+++ time.js
@@ -2,16 +2,32 @@
 
 let myTime=document.getElementById('myTime');
 	myTime.addEventListener('click',funcTime);
-	function stop(){
-		window.clearInterval(window.timerId)
+
+	let stopTime=document.getElementById('stopTime');
+		stopTime.addEventListener('click',stopTimres);
+	function stopTimres(){
+		window.clearInterval(window.timerId);
+		
+		document.body.style.background='green';
+		if(document.body.style.background=='green'){
+			let blockTimer=document.getElementById('block')
+			blockTimer.style.background='yellow';
+
+		}
+
 	}
+	function stop(){
+		window.clearInterval(window.timerId);
+	}
+	
 	let numberTimer=document.getElementById('numberTimer');
 			numberTimer.addEventListener('mouseout',timerNumber);
 			function timerNumber(){
 				timer.innerHTML=numberTimer.value;
 			}
+
 	function funcTime(){
-		window.timerId=window.setInterval(timers,300)
+		window.timerId=window.setInterval(timers,1000)
 		function timers(){
 			let timer=document.getElementById('timer');
 
